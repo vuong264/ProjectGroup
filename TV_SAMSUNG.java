c5a641fcad21d95e1434c7a258c7d37512db4398
hoang <hoangdeptrai2611@gmail.com>
2024-04-03 10:49:55 +0700

    Add Format

--- TV_SAMSUNG.java
+++ TV_SAMSUNG.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-public class TV_SAMSUNG extends TV{
+public class TV_SAMSUNG extends TV {
     private String state;
 
     public TV_SAMSUNG() {
@@ -12,21 +12,21 @@ public class TV_SAMSUNG extends TV{
     }
 
     @Override
-    public void Input(){
+    public void Input() {
         super.Input();
         System.out.print("Moi nhap State (new or old): ");
         sc.nextLine();
         state = sc.nextLine();
     }
-    
+
     @Override
-    public void Output(){
+    public void Output() {
         super.Output();
-        System.out.println("State: "+state);
-        System.out.println("Total: "+sum_Total());
-        System.out.println("Discount: "+discount());
+        System.out.println("State: " + state);
+        System.out.println("Total: " + sum_Total());
+        System.out.println("Discount: " + discount());
     }
-    
+
     public String getState() {
         return state;
     }
@@ -34,21 +34,19 @@ public class TV_SAMSUNG extends TV{
     public void setState(String state) {
         this.state = state;
     }
-    
+
     @Override
-    public double sum_Total(){
-        return getNum()*getPrice()-discount();
+    public double sum_Total() {
+        return getNum() * getPrice() - discount();
     }
-    
+
     @Override
-    public double discount(){
-        if("new".equals(state)){
-            return getNum()*getPrice()*10/100;
-        }
-        else if("old".equals(state)){
-            return getNum()*getPrice()*60/100;
-        }
-        else return 0;
+    public double discount() {
+        if ("new".equals(state)) {
+            return getNum() * getPrice() * 10 / 100;
+        } else if ("old".equals(state)) {
+            return getNum() * getPrice() * 60 / 100;
+        } else
+            return 0;
     }
 }
-
