bd724aa4da90103f66b9940941609cf4bb94e8c9
acer <acer@172.25.236.70>
2024-04-03 10:33:29 +0700

    Completed Task4

--- TV_SONY.java
+++ TV_SONY.java
@@ -1,5 +1,46 @@
-package project;
+import java.util.Date;
 
-public class TV_SONY {
+public class TV_SONY extends TV{
+    private double surcharge;
 
+    public TV_SONY() {
+    }
+
+    public TV_SONY(double surcharge, String tvID, String manifacturer, Date entryDate, double price, double num) {
+        super(tvID, manifacturer, entryDate, price, num);
+        this.surcharge = surcharge;
+    }
+
+    @Override
+    public void Input(){
+        super.Input();
+        System.out.print("Moi nhap Surcharge: ");
+        surcharge = sc.nextDouble();
+    }
+    
+    @Override
+    public void Output(){
+        super.Output();
+        System.out.println("Surcharge: "+surcharge);
+        System.out.println("Total: "+sum_Total());
+        System.out.println("Discount: "+discount());
+    }
+    
+    public double getSurcharge() {
+        return surcharge;
+    }
+
+    public void setSurcharge(double surcharge) {
+        this.surcharge = surcharge;
+    }
+    
+    @Override
+    public double sum_Total(){
+        return getNum()*getPrice()+surcharge-discount();
+    }
+    
+    @Override
+    public double discount(){
+        return getNum()*getPrice()*5/100;
+    }
 }
